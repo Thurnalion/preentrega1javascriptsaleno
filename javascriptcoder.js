2da20944942d26ccea33e953d28f3deb126dde39
unknown <nicosaleno@gmail.com>
2023-01-12 17:31:26 -0300

    12/1

--- javascriptcoder.js
+++ javascriptcoder.js
@@ -36,6 +36,19 @@ if(final === efeencaja){
 
 }
 
+function inicio(){
+
+    let user, pass
+
+    user = document.getElementById("usuario").value;
+    pass = document.getElementById("contrseña").value;
+
+    if(user == "nico" && pass == "1010"){
+    window.location = "presentismo.html";
+    } else{
+    alert("Datos incorrectos.");
+    }
+}
 
 /* Esto funciona pero no esta daptado 
 
